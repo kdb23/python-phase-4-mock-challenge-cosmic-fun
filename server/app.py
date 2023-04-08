3765a2ee54ca0acf6398924b0544d98b15fa9f52
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-04-08 15:31:40 -0400

    model creation, restful api routes, get request

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_restful import Api, Resource
 
 from models import db, Scientist, Planet, Mission
 
@@ -12,15 +13,58 @@ app.json.compact = False
 CORS(app)
 migrate = Migrate(app, db)
 
+
 db.init_app(app)
+api = Api(app)
+
+class Index(Resource):
+    def get(self):
+        response = make_response(
+            {"message": "Hello Scientists!"}
+        )
+        return response
+
+api.add_resource(Index, '/')
+
+class Scientists(Resource):
+    def get(self):
+        smart_kid = [s.to_dict() for s in Scientist.query.all()]
+        return make_response(jsonify(smart_kid), 200)
+
+    def post(self):
+        data = request.get_json()
+        new_kid = Scientist(
+            name = data['name'],
+            field_of_study = data['field_of_study'],
+            avatar = data['avatar'],
+        )
+        db.session.add(new_kid)
+        db.session.commit()
+
+        return make_response(new_kid.to_dict(), 201)
 
 
-@app.route('/')
-def index():
-    response = make_response(
-        {"message": "Hello Scientists!"}
-    )
+api.add_resource(Scientists, '/scientists')
 
+class ScientistsById(Resource):
+    def get(self, id):
+        smart_kid = Scientist.query.filter_by(id = id).first().to_dict()
+        return make_response(jsonify(smart_kid), 200)
+    
+    def patch(self,id):
+        data = request.get_json()
+        new_info = Scientist.query.filter_by(id = id).first()
+        for info in data:
+            setattr(new_info, info, data[info])
+
+        db.session.add(new_info)
+        db.session.commit()
+
+        new_info_dict = new_info.to_dict()
+        response = make_response(new_info_dict, 200)
+        return response
+
+api.add_resource(ScientistsById, '/scientists/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -14,16 +14,55 @@ db = SQLAlchemy(metadata=metadata)
 class Mission(db.Model, SerializerMixin):
     __tablename__ = 'missions'
 
-    id = db.Column(db.Integer, primary_key=True)
+    serialize_rules = ('-scientist.missions', '-planet.missions',)
 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    scientist_id = db.Column(db.Integer, db.ForeignKey('scientists.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+
+    def __repr__(self):
+        return f'<Mission id={self.id}, name={self.name}, scientist_id={self.scientist_id}, planet_id={self.planet_id}>'
 
 class Scientist(db.Model, SerializerMixin):
     __tablename__ = 'scientists'
 
+    serialize_rules = ('-missions.scientist',)
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    field_of_study = db.Column(db.String)
+    avatar = db.Column(db.String)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    missions = db.relationship("Mission", backref = 'scientist')
+
+    def __repr__(self):
+        return f'<Scientist {self.name}, {self.field_of_study}, {self.avatar}>'
+    
 
 
 class Planet(db.Model, SerializerMixin):
     __tablename__ = 'planets'
 
+    serialize_rules = ('-missions.planet',)
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    distance_from_earth = db.Column(db.String)
+    nearest_star = db.Column(db.String)
+    image = db.Column(db.String)
+    
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    missions = db.relationship("Mission", backref = 'planet')
+
+    def repr__(self):
+        return f'<Planet {self.name}, {self.distance_from_earth}, {self.nearest_star}, {self.image}>'
